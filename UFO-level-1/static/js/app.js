ebf762db7957001087fadaa4ef64cc66b2155758
rmotta1992 <68138899+rmotta1992@users.noreply.github.com>
2020-11-19 14:29:44 -0500

    adding js code to input data into table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,24 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// reference the table body
+var tbody = d3.select("tbody");
+
+// Console.log the data to check it pulls correctly 
+console.log(tableData);
+
+// // Step 1: Loop Through `data` and console.log each weather report object
+// data.forEach(function(ufoSighting) {
+//   console.log(ufoSighting);
+// });
+
+// update cell text with data from file 
+tableData.forEach(function(ufoSighting) {
+  console.log(ufoSighting);
+  var row = tbody.append("tr");
+  Object.entries(ufoSighting).forEach(function([key, value]) {
+    console.log(key, value);
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
