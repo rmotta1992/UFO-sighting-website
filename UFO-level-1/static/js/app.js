a053a8f333b053c90fd82d68f4b46b8d92dfe6b1
rmotta1992 <68138899+rmotta1992@users.noreply.github.com>
2020-11-21 18:14:40 -0500

    adding code for filtering the table to search dates

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -6,7 +6,7 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 // Console.log the data to check it pulls correctly 
-//console.log(tableData);
+console.log(tableData);
 
 // // Step 1: Loop Through `data` and console.log each weather report object
 // data.forEach(function(ufoSighting) {
@@ -89,5 +89,53 @@ tableData.forEach(function(ufoSighting) {
     // Set the span tag in the h1 element to the text
     // that was entered in the form
     //d3.select("h1>span").text(inputValue);
-  }
+    //if inputValue === 'Date':
+      //  console.log(1)
+    //   function myFunction() {
+    //     var input, filter, table, tr, td, i, txtValue;
+    //     input = inputValue;
+    //     filter = input.value.toUpperCase();
+    //     table = document.getElementById("ufo-table");
+    //     tr = table.getElementsByTagName("tr");
+    //     for (i = 0; i < tr.length; i++) {
+    //       td = tr[i].getElementsByTagName("td")[0];
+    //       if (td) {
+    //         txtValue = td.textContent || td.innerText;
+    //         if (txtValue.toUpperCase().indexOf(filter) > -1) {
+    //           tr[i].style.display = "";
+    //         } else {
+    //           tr[i].style.display = "none";
+    //         }
+    //       }       
+    //     }
+    //   } 
+    // function searchDate(newdate) {
+    //     // return player.madeTeam == true;
+    //     // A more concise way to express a boolean conditional
+    //     return newdate.datetime;
+    //   }
+    //   var inputResults = data.filter(searchDate);
+    // Create a custom filtering function
+    var newTalbe = []
+    function selectDate(date) {
+        return date.datetime === inputValue;
+    }
+  
+    // filter() uses the custom function as its argument
+    var userDate = tableData.filter(selectDate);
+  
+    // Test
+    newTalbe.append(userDate);
+    
+    // tableData.forEach(function(ufoSighting) {
+    //     //console.log(ufoSighting);
+    //     var row = tbody.append("tr");
+    //     Object.entries(ufoSighting).forEach(function([key, value]) {
+    //       //console.log(key, value);
+    //       var cell = row.append("td");
+    //       cell.text(value);
+    //     });
+    //   });
+}
+
 
