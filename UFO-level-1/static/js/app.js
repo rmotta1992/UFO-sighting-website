68f7b573db1edace24f3bb9fc1a1a9a4a28392ea
rmotta1992 <68138899+rmotta1992@users.noreply.github.com>
2020-11-21 18:24:43 -0500

    cleaning the file

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -24,44 +24,6 @@ tableData.forEach(function(ufoSighting) {
   });
 });
 
-//var text = d3.select("#datetime");
-//var output = d3.select(".output");
-
-// function handleSubmit() {
-//     // Prevent the page from refreshing
-//     d3.event.preventDefault();
-  
-//     // Select the input value from the form
-//     var date = d3.select("#datetime").node().value;
-//     console.log(date);
-  
-//     // clear the input value
-//     d3.select("#datetime").node().value = "";
-//   // Build the plot with the new stock
-//     buildPlot(date);
-// }
-// d3.select("#submit").on("click", handleSubmit);
-
-// var input, filter, table, tr, td, i, txtValue;
-//   input = document.getElementById("myInput");
-//   //filter = input.value.toUpperCase();
-//   table = document.getElementById("myTable");
-//   tr = table.getElementsByTagName("tr");
-
-//   // Loop through all table rows, and hide those who don't match the search query
-//   for (i = 0; i < tr.length; i++) {
-//     td = tr[i].getElementsByTagName("td")[0];
-//     if (td) {
-//       txtValue = td.textContent || td.innerText;
-//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//         tr[i].style.display = "";
-//       } else {
-//         tr[i].style.display = "none";
-//       }
-//     }
-//   }
-
-
   var button = d3.select("#filter-btn");
 
   // Select the form
@@ -125,17 +87,11 @@ tableData.forEach(function(ufoSighting) {
     var userDate = tableData.filter(selectDate);
   
     // Test
-    newTalbe.append(userDate);
+    console.log(userDate);
     
-    // tableData.forEach(function(ufoSighting) {
-    //     //console.log(ufoSighting);
-    //     var row = tbody.append("tr");
-    //     Object.entries(ufoSighting).forEach(function([key, value]) {
-    //       //console.log(key, value);
-    //       var cell = row.append("td");
-    //       cell.text(value);
-    //     });
-    //   });
+    // if tableData.datetime === inputValue:  
+    //     newTalbe.append(userDate)
+  
 }
 
 
