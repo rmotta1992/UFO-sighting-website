c62a46f1150a6bfe94e8e1e6a9ba6a8f239ac2c6
rmotta1992 <68138899+rmotta1992@users.noreply.github.com>
2020-11-21 19:23:07 -0500

    adding code to have table show only dates entered in search field

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -48,50 +48,32 @@ tableData.forEach(function(ufoSighting) {
     // Print the value to the console
     console.log(inputValue);
   
-    // Set the span tag in the h1 element to the text
-    // that was entered in the form
-    //d3.select("h1>span").text(inputValue);
-    //if inputValue === 'Date':
-      //  console.log(1)
-    //   function myFunction() {
-    //     var input, filter, table, tr, td, i, txtValue;
-    //     input = inputValue;
-    //     filter = input.value.toUpperCase();
-    //     table = document.getElementById("ufo-table");
-    //     tr = table.getElementsByTagName("tr");
-    //     for (i = 0; i < tr.length; i++) {
-    //       td = tr[i].getElementsByTagName("td")[0];
-    //       if (td) {
-    //         txtValue = td.textContent || td.innerText;
-    //         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-    //           tr[i].style.display = "";
-    //         } else {
-    //           tr[i].style.display = "none";
-    //         }
-    //       }       
-    //     }
-    //   } 
-    // function searchDate(newdate) {
-    //     // return player.madeTeam == true;
-    //     // A more concise way to express a boolean conditional
-    //     return newdate.datetime;
-    //   }
-    //   var inputResults = data.filter(searchDate);
-    // Create a custom filtering function
+    // filter data by user input 
     var newTalbe = []
     function selectDate(date) {
         return date.datetime === inputValue;
     }
-  
-    // filter() uses the custom function as its argument
     var userDate = tableData.filter(selectDate);
   
     // Test
     console.log(userDate);
-    
+   
     // if tableData.datetime === inputValue:  
     //     newTalbe.append(userDate)
-  
+    var tbody2 = d3.select("tbody")
+
+    tbody.html("");
+
+    userDate.forEach(function(selectDate) {
+        //console.log(ufoSighting);
+        var row = tbody.append("tr");
+        Object.entries(selectDate).forEach(function([key, value]) {
+          //console.log(key, value);
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
 }
 
 
+  
